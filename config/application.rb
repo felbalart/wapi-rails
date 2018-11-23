c4209ad62eaa2b42dc1f349323ab04a85e375437
Felipe Balart <felbalart@gmail.com>
2018-11-23 18:21:54 -0300

    feat(): this is the famous initial commit

---
+++ config/application.rb
@@ -0,0 +1,13 @@
+require_relative 'boot'
+
+require 'rails/all'
+Bundler.require(*Rails.groups)
+
+module WapiRails
+  class Application < Rails::Application
+    config.i18n.fallbacks = [:es, :en]
+    config.i18n.default_locale = 'en'
+    config.assets.paths << Rails.root.join('node_modules')
+    config.load_defaults 5.2
+  end
+end
